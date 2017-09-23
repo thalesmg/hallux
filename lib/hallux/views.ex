97fa8fefe8cad12018d05ff984db7f1d9a493f1d
Thales Macedo Garitezi <thalesmg@gmail.com>
2017-09-23 17:42:50 -0300

    Enumerable implementations for Views.

--- lib/hallux/views.ex
+++ lib/hallux/views.ex
@@ -11,3 +11,76 @@ defmodule Hallux.Views do
   def nilR(), do: %__MODULE__.NilR{}
   def consR(hd, tl), do: %__MODULE__.ConsR{hd: hd, tl: tl}
 end
+
+
+defimpl Enumerable, for: Hallux.Views.NilL do
+  def count(_nilL), do: {:ok, 0}
+  def member?(_nilL, _elem), do: {:ok, false}
+
+  def reduce(_nilL, {:halt, acc}, _fun) do
+    {:halted, acc}
+  end
+  def reduce(nilL, {:suspend, acc}, fun) do
+    {:suspended, acc, & fun.(nilL, &1, fun)}
+  end
+  def reduce(_nilL, {:cont, acc}, _fun) do
+    {:done, acc}
+  end
+end
+
+defimpl Enumerable, for: Hallux.Views.ConsL do
+  def count(consL), do: {:error, __MODULE__}
+  def member?(consL, elem), do: {:error, __MODULE__}
+
+  def reduce(_consL, {:halt, acc}, _fun) do
+    {:halted, acc}
+  end
+  def reduce(consL, {:suspend, acc}, fun) do
+    {:suspended, acc, & fun.(consL, &1, fun)}
+  end
+  def reduce(consL, {:cont, acc}, fun) do
+    case Hallux.viewL(consL.tl) do
+      %Hallux.Views.NilL{} ->
+        {:cont, result} = fun.(consL.hd, acc)
+        {:done, result}
+      %Hallux.Views.ConsL{} = tl ->
+        reduce(tl, fun.(consL.hd, acc), fun)
+    end
+  end
+end
+
+defimpl Enumerable, for: Hallux.Views.NilR do
+  def count(_nilR), do: {:ok, 0}
+  def member?(_nilR, _elem), do: {:ok, false}
+
+  def reduce(_nilR, {:halt, acc}, _fun) do
+    {:halted, acc}
+  end
+  def reduce(nilR, {:suspend, acc}, fun) do
+    {:suspended, acc, & fun.(nilR, &1, fun)}
+  end
+  def reduce(_nilR, {:cont, acc}, _fun) do
+    {:done, acc}
+  end
+end
+
+defimpl Enumerable, for: Hallux.Views.ConsR do
+  def count(consR), do: {:error, __MODULE__}
+  def member?(consR, elem), do: {:error, __MODULE__}
+
+  def reduce(_consR, {:halt, acc}, _fun) do
+    {:halted, acc}
+  end
+  def reduce(consR, {:suspend, acc}, fun) do
+    {:suspended, acc, & fun.(consR, &1, fun)}
+  end
+  def reduce(consR, {:cont, acc}, fun) do
+    case Hallux.viewR(consR.tl) do
+      %Hallux.Views.NilR{} ->
+        {:cont, result} = fun.(consR.hd, acc)
+        {:done, result}
+      %Hallux.Views.ConsR{} = tl ->
+        reduce(tl, fun.(consR.hd, acc), fun)
+    end
+  end
+end
---
+++ test/hallux/views_test.exs
@@ -0,0 +1,76 @@
+defmodule Hallux.ViewsTest do
+  use ExUnit.Case
+
+  alias Hallux
+  alias Hallux.Views
+  import Hallux.Views, only: [
+    nilL: 0,
+    nilR: 0
+  ]
+
+  describe "Enumerable Left" do
+    test "count : nil" do
+      assert 0 == Enum.count(nilL())
+    end
+
+    test "count : cons" do
+      tree = Hallux.to_tree(1..10)
+      assert 10 == Enum.count(Hallux.viewL(tree))
+    end
+
+    test "member? : nil" do
+      assert false == Enum.member?(nilL(), 1)
+    end
+
+    test "member? : cons" do
+      tree = Hallux.to_tree(1..10)
+      target = Enum.random(1..10)
+      assert true == Enum.member?(Hallux.viewL(tree), target)
+    end
+
+    test "reduce : nil" do
+      assert_raise Enum.EmptyError, fn ->
+        Enum.reduce(nilL(), & &1 + 1)
+      end
+    end
+
+    test "reduce : cons" do
+      tree = Hallux.to_tree(1..10)
+      list = Enum.to_list(1..10)
+      assert list == Enum.to_list(Hallux.viewL(tree))
+    end
+  end
+
+  describe "Enumerable Right" do
+    test "count : nil" do
+      assert 0 == Enum.count(nilR())
+    end
+
+    test "count : cons" do
+      tree = Hallux.to_tree(1..10)
+      assert 10 == Enum.count(Hallux.viewR(tree))
+    end
+
+    test "member? : nil" do
+      assert false == Enum.member?(nilR(), 1)
+    end
+
+    test "member? : cons" do
+      tree = Hallux.to_tree(1..10)
+      target = Enum.random(1..10)
+      assert true == Enum.member?(Hallux.viewR(tree), target)
+    end
+
+    test "reduce : nil" do
+      assert_raise Enum.EmptyError, fn ->
+        Enum.reduce(nilR(), & &1 + 1)
+      end
+    end
+
+    test "reduce : cons" do
+      tree = Hallux.to_tree(1..10)
+      list = Enum.to_list(10..1)
+      assert list == Enum.to_list(Hallux.viewR(tree))
+    end
+  end
+end
