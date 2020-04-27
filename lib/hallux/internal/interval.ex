63c935a57acc124d1931d36a9fc6b03df9c79d34
Thales Macedo Garitezi <thalesmg@gmail.com>
2020-04-27 13:18:58 -0300

    mais rasc

--- lib/hallux/internal/interval.ex
+++ lib/hallux/internal/interval.ex
@@ -6,14 +6,24 @@ defmodule Hallux.Internal.Interval do
     alias Hallux.Internal.Key.Key
     alias Hallux.Internal.MTuple
     alias Hallux.Internal.Prio.Prio
+    alias Hallux.Internal.IntInterval.IntInterval
+    alias Hallux.Internal.IntInterval.NoInterval
     alias Hallux.Protocol.Monoid
 
+    # def size(%Interval{low: low, high: high}) do
+    #   %MTuple{a: %Key{k: low}, b: %Prio{p: high}}
+    # end
+
+    # def monoid_type(%Interval{low: low, high: high}) do
+    #   Monoid.mempty(%MTuple{a: low, b: high})
+    # end
+
     def size(%Interval{low: low, high: high}) do
-      %MTuple{a: %Key{k: low}, b: %Prio{p: high}}
+      %IntInterval{i: {low, high}, v: high}
     end
 
     def monoid_type(%Interval{low: low, high: high}) do
-      Monoid.mempty(%MTuple{a: low, b: high})
+      Monoid.mempty(%NoInterval{})
     end
   end
 end
--- lib/hallux/internal/mtuple.ex
+++ lib/hallux/internal/mtuple.ex
@@ -1,6 +1,23 @@
 defmodule Hallux.Internal.MTuple do
   defstruct [:a, :b]
 
+  alias Hallux.Internal.Key, as: KeyBase
+  alias Hallux.Internal.Key.Key
+  alias Hallux.Internal.Prio, as: PrioBase
+  alias Hallux.Internal.Prio.Prio
+
+  def new({l, h}) do
+    %__MODULE__{a: %Key{k: l}, b: %Prio{p: h}}
+  end
+
+  def compare(%__MODULE__{a: key1, b: prio1}, %__MODULE__{a: key2, b: prio2}) do
+    case {KeyBase.compare(key1, key2), PrioBase.compare(prio1, prio2)} do
+      {:lt, _} -> :lt
+      {:gt, _} -> :gt
+      {:eq, cmp} -> cmp
+    end
+  end
+
   defimpl Hallux.Protocol.Monoid do
     alias Hallux.Internal.MTuple
     alias Hallux.Protocol.Monoid
