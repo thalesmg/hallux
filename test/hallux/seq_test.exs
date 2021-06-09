073ff2ce314398a7b3a4effcd9c583348077af25
Manuel Bärenz <m.baerenz@sonnen.de>
2021-06-09 16:14:20 +0200

    Add more properties to Seq

--- test/hallux/seq_test.exs
+++ test/hallux/seq_test.exs
@@ -40,6 +40,37 @@ defmodule Hallux.SeqTest do
         assert Valid.valid?(seq)
       end
     end
+
+    property "cons . view_l = id" do
+      check all(
+              s <- seq(),
+              item <- term()
+            ) do
+        {item_, s_} = Seq.view_l(Seq.cons(s, item))
+        assert_equal(s, s_)
+        assert item == item_
+      end
+    end
+  end
+
+  property "snoc . view_r = id" do
+    check all(
+            s <- seq(),
+            item <- term()
+          ) do
+      {s_, item_} = Seq.view_r(Seq.snoc(s, item))
+      assert_equal(s, s_)
+      assert item == item_
+    end
+  end
+
+  property "concat . to_list = to_list . ++" do
+    check all(
+      s1 <- seq(),
+      s2 <- seq()
+    ) do
+      assert Enum.to_list(Seq.concat(s1, s2)) == Enum.to_list(s1) ++ Enum.to_list(s2)
+    end
   end
 
   defp assert_equal(s1 = %Seq{}, s2 = %Seq{}) do
