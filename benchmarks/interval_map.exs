320d7a528dfbee94ede6c57755f1a80c25768fc3
Thales Macedo Garitezi <thalesmg@gmail.com>
2021-06-23 09:41:29 -0300

    format bechmarks

--- benchmarks/interval_map.exs
+++ benchmarks/interval_map.exs
@@ -1,30 +1,38 @@
 alias Hallux.IntervalMap
 
 make_low_high_pair = fn n ->
-    start = :rand.uniform(n)
-    length = :rand.uniform(n)
-    {start, start + length}
-  end
+  start = :rand.uniform(n)
+  length = :rand.uniform(n)
+  {start, start + length}
+end
 
 make_test_input = fn n ->
-    im = for _ <- 1..n do
+  im =
+    for _ <- 1..n do
       {make_low_high_pair.(n), nil}
     end
     |> Enum.into(IntervalMap.new())
-    {im, n}
-  end
+
+  {im, n}
+end
 
 inputs = %{
   "tiny interval map" => make_test_input.(1),
   "small interval map" => make_test_input.(100),
-  "medium interval map" => make_test_input.(10_000),
+  "medium interval map" => make_test_input.(10_000)
 }
 
 Benchee.run(
   %{
-    "insert" => fn {interval_map, n} -> IntervalMap.insert(interval_map, make_low_high_pair.(n)) end,
-    "interval_search" => fn {interval_map, n} -> IntervalMap.interval_search(interval_map, make_low_high_pair.(n)) end,
-    "interval_match" => fn {interval_map, n} -> IntervalMap.interval_match(interval_map, make_low_high_pair.(n)) end
+    "insert" => fn {interval_map, n} ->
+      IntervalMap.insert(interval_map, make_low_high_pair.(n))
+    end,
+    "interval_search" => fn {interval_map, n} ->
+      IntervalMap.interval_search(interval_map, make_low_high_pair.(n))
+    end,
+    "interval_match" => fn {interval_map, n} ->
+      IntervalMap.interval_match(interval_map, make_low_high_pair.(n))
+    end
   },
   inputs: inputs,
   unit_scaling: :none
